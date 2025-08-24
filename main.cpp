b22bcf0208f038202be3c41c9d0dce518860342c
jmcandido <joaomarcelocandido42@gmail.com>
2025-08-24 10:30:12 -0300

    iniciando estrutura do guloso

--- main.cpp
+++ main.cpp
@@ -4,10 +4,13 @@
 using namespace std;
 
 
+struct Rota 
+{
+    vector<int> caminho;
+    int custo;
+};
 
-void exibirDados(int n, int m, int Q, 
-                  const vector<int>& d, 
-                  const vector<vector<int> >& c){
+void exibirDados(int n, int m, int Q, const vector<int>& d, const vector<vector<int> >& c){
     cout << "n = " << n << endl;
     cout << "m = " << m << endl;
     cout << "Q = " << Q << endl;
@@ -28,7 +31,6 @@ void exibirDados(int n, int m, int Q,
 
 }
 
-
 int main() {
     ifstream in("instancia.txt"); // arquivo de entrada
     if (!in) {
@@ -40,6 +42,7 @@ int main() {
     in >> n >> m >> Q;
 
     vector<int> d(n);
+    d[0] = 0;
     for (int i = 0; i < n; i++) {
         in >> d[i];
     }
@@ -51,10 +54,23 @@ int main() {
         }
     }
 
-    // Exibir dados lidos
-    
-    exibirDados(n,m,Q,d,c);
+    int coletas, entregas = 0;
 
+    for(int i = 1; i <= n; i++) {
+        if(d[i] > 0)
+            coletas += d[i];
+        else
+            entregas += abs(d[i]);
+    }
+
+    int deficit = max(0, entregas - coletas);
+
+    cout << "Total de coletas  = " << coletas << "\n";
+    cout << "Total de entregas  = " << entregas << "\n";
+    cout << "Déficit  = " << deficit << "\n";
+
+
+    vector<bool> visitado(n+1, false);
 
     return 0;
 }
